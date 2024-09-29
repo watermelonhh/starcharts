b313255cecf162da3384a342d55e0025e11fadbd
watermelonhh <53829603+watermelonhh@users.noreply.github.com>
2024-09-29 22:00:57 +0800

    Update main.go
    9/29  加了点注释

--- main.go
+++ main.go
@@ -20,13 +20,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-//go:embed static/*
+//go:embed static/*   这行代码表示将 static 目录下的所有文件嵌入到 static 变量中。然后，可以通过 static 来访问这些文件的内容。
 var static embed.FS
 
 var version = "devel"
 
 func main() {
-	log.SetHandler(text.New(os.Stderr))
+	log.SetHandler(text.New(os.Stderr)) //这是日志处理器，日志输出到标准错误输出
 	// log.SetLevel(log.DebugLevel)
 	config := config.Get()
 	ctx := log.WithField("listen", config.Listen)
@@ -34,17 +34,17 @@ func main() {
 	if err != nil {
 		log.WithError(err).Fatal("invalid redis_url")
 	}
-	redis := redis.NewClient(options)
-	cache := cache.New(redis)
+	redis := redis.NewClient(options) //创建一个redis客户端
+	cache := cache.New(redis) //创建缓存，在main结束时结束缓存
 	defer cache.Close()
-	github := github.New(config, cache)
+	github := github.New(config, cache) //初始化 GitHub 客户端，传入配置和缓存实例。
 
-	r := mux.NewRouter()
+	r := mux.NewRouter()  //创建一个路由器，用来定义一个路由
 	r.Path("/").
-		Methods(http.MethodGet).
-		Handler(controller.Index(static, version))
+		Methods(http.MethodGet).   //这里是get
+		Handler(controller.Index(static, version))   //从static文件里面拿东西出来显示到网页上
 	r.Path("/").
-		Methods(http.MethodPost).
+		Methods(http.MethodPost).   //这里是post
 		HandlerFunc(controller.HandleForm())
 	r.PathPrefix("/static/").
 		Methods(http.MethodGet).
@@ -56,7 +56,7 @@ func main() {
 		Methods(http.MethodGet).
 		Handler(controller.GetRepo(static, github, cache, version))
 
-	// generic metrics
+	// generic metrics   创建一个 Prometheus 计数器，用于统计请求总数，按状态码和请求方法分组
 	requestCounter := promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "starcharts",
 		Subsystem: "http",
