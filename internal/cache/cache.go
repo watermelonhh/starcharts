f98bb6fce36f467cc84d7bae7c134fcb57ad99a8
watermelonhh <53829603+watermelonhh@users.noreply.github.com>
2024-10-01 23:29:58 +0800

    Update cache.go

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,7 +7,7 @@ import (
 	msgpack "gopkg.in/vmihailenco/msgpack.v2"
 )
 
-// nolint: gochecknoglobals
+// nolint: gochecknoglobals   新建一个计数器。读数据的计数器
 var cacheGets = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "starcharts",
@@ -17,7 +17,7 @@ var cacheGets = prometheus.NewCounter(
 	},
 )
 
-// nolint: gochecknoglobals
+// nolint: gochecknoglobals   新建一个计数器。存数据的计数器
 var cachePuts = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "starcharts",
@@ -27,7 +27,7 @@ var cachePuts = prometheus.NewCounter(
 	},
 )
 
-// nolint: gochecknoglobals
+// nolint: gochecknoglobals  删除缓存的计数器
 var cacheDeletes = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "starcharts",
@@ -42,20 +42,20 @@ func init() {
 	prometheus.MustRegister(cacheGets, cachePuts, cacheDeletes)
 }
 
-// Redis cache.
+// Redis cache. 
 type Redis struct {
-	redis *redis.Client
-	codec *rediscache.Codec
+	redis *redis.Client  //redis客户端
+	codec *rediscache.Codec //对象序列化/反序列化成二进制数据的编解码器
 }
 
 // New redis cache.
 func New(redis *redis.Client) *Redis {
 	codec := &rediscache.Codec{
 		Redis: redis,
-		Marshal: func(v interface{}) ([]byte, error) {
+		Marshal: func(v interface{}) ([]byte, error) { //将数据变成字节码，序列化
 			return msgpack.Marshal(v)
 		},
-		Unmarshal: func(b []byte, v interface{}) error {
+		Unmarshal: func(b []byte, v interface{}) error { //反序列化过程
 			return msgpack.Unmarshal(b, v)
 		},
 	}
@@ -71,18 +71,18 @@ func (c *Redis) Close() error {
 	return c.redis.Close()
 }
 
-// Get from cache by key.
+// Get from cache by key.  从缓存中读取数据。
 func (c *Redis) Get(key string, result interface{}) error {
-	if err := c.codec.Get(key, result); err != nil {
+	if err := c.codec.Get(key, result); err != nil {  //把redis里面的结果反序列化，然后放到result中
 		return err
 	}
-	cacheGets.Inc()
+	cacheGets.Inc()  //计数器+1 
 	return nil
 }
 
 // Put on cache.
 func (c *Redis) Put(key string, obj interface{}) error {
-	if err := c.codec.Set(&rediscache.Item{
+	if err := c.codec.Set(&rediscache.Item{ // 把值序列化并且 存到 redis中
 		Key:    key,
 		Object: obj,
 	}); err != nil {
